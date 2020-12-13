99ea1668e30a18642f6cf4f87a1e9a7a50109697
Danny Fritz <drfritz@csbsju.edu>
2020-12-13 16:22:03 -0600

    please work

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.set('port', process(process.env.PORT || port ));
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(app.get('port'), function () {
+app.listen(port, function () {
     console.log(`App running on http://localhost:${port}`);
 });
