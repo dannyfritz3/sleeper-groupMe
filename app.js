3819bbd4a5e21cdb5bb1a55ebf48cae53d8f34c2
Danny Fritz <drfritz@csbsju.edu>
2020-12-13 16:35:43 -0600

    testing axios call

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const url = "https://api.sleeper.app/v1/league/575392779379286016"
+var sleeperResponse;
+
+axios.get(url).then((response) => {
+    sleeperResponse = response;
+}, (error) => {
+    sleeperResponse = error;
+});
+
+app.get('/', (req, res) => res.send(sleeperResponse));
 
 app.listen(port, function () {
     console.log(`App running on http://localhost:${port}`);
