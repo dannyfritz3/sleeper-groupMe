bfe4442a2cee993a123ed90913946d3c45ab33d7
Danny Fritz <drfritz@csbsju.edu>
2020-12-13 16:44:27 -0600

    fixing async stuff

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const axios = require('axios');
 const app = express();
 const port = 8080;
 
-const url = "https://api.sleeper.app/v1/league/575392779379286016"
-var sleeperResponse = await axios.get(url);
-
-app.get('/', (req, res) => res.json(sleeperResponse));
+app.get('/', (req, res) => res.json(getSleeperResponse()));
 
 app.listen(port, function () {
     console.log(`App running on http://localhost:${port}`);
 });
+
+async function getSleeperResponse() {
+    const url = "https://api.sleeper.app/v1/league/575392779379286016"
+    var response = await axios.get(url);
+    return response;
+}
