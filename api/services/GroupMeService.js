ad5ea56a970a7fc2650e2b73fbb64badf1737a56
Danny Fritz <drfritz@csbsju.edu>
2020-12-14 15:47:19 -0600

    Trying to fix the lowercase g in this filename

---
+++ api/services/GroupMeService.js
@@ -0,0 +1,31 @@
+//var events = require('events');
+//var eventEmitter = new events.EventEmitter();
+
+class GroupMeService {
+    constructor() {};
+
+    handleCallback(req, res) {
+        if(this.invoked(req)){
+            this.parseMessage(req.text)
+            res.sendStatus(202)
+        } else {
+            res.sendStatus(200);
+        };
+    }
+    
+    invoked (req) {
+        try {        
+            var request = JSON.parse(req.chunks[0]);
+            var botRegex = /(?i)sleeperbot/;
+            return req.text && botRegex.test(request.text);
+        } catch(error) {
+            return false;
+        };
+    }
+
+    parseMessage (message) {
+        console.log(message);
+    }
+};
+
+module.exports = GroupMeService;
