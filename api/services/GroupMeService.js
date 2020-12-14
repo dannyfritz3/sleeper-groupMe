19502a4dd1a19da91f8ab77ac64d9c962534b84f
Danny Fritz <drfritz@csbsju.edu>
2020-12-14 16:23:31 -0600

    tyrying to see groupme post request body

--- api/services/GroupMeService.js
+++ api/services/GroupMeService.js
@@ -20,7 +20,7 @@ class GroupMeService {
     invoked (req) {
         try {        
             //var request = JSON.parse(req.chunks[0]);
-            console.log("MESSAGE RECEIVED FROM GROUPME: " + req);
+            console.log("MESSAGE RECEIVED FROM GROUPME: " + req.body);
             var botRegex = /(?i)@sleeperbot/;
             return req.text && botRegex.test(request.text);
         } catch(error) {
