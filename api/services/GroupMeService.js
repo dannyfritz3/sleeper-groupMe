d6407580aebbd6eee94db7f96b7e6424c71c8d6b
Danny Fritz <drfritz@csbsju.edu>
2020-12-14 23:01:57 -0600

    maybe this will work

--- api/services/GroupMeService.js
+++ api/services/GroupMeService.js
@@ -1,6 +1,7 @@
 //var events = require('events');
 //var eventEmitter = new events.EventEmitter();
 const GroupMeAdapter = require('../adapters/GroupMeAdapter.js');
+var regex = require('regex');
 
 var groupMeAdapter;
 
@@ -13,7 +14,7 @@ class GroupMeService {
     //TODO add model for jsonMessage coming in from GroupMe
     handleCallback(req, res) {
         var jsonMessage = JSON.parse(JSON.stringify(req.body));
-        var botRegex = new RegExp('(?i)@sleeperbot');
+        var botRegex = new regex(/(?i)@sleeperbot/);
         var test = botRegex.test(jsonMessage.text);
         console.log("GroupMe message text: " + jsonMessage.text + "\nRegEx Match: " + test);
         if(test) {
@@ -21,16 +22,16 @@ class GroupMeService {
         };
     }
     
-    invoked (messageText) {
-        try {        
-            var botRegex = /(?i)@sleeperbot/;
-            var test = messageText && botRegex.test(messageText);
-            console.log("RegEx Test: " + test);
-            return messageText && botRegex.test(messageText);
-        } catch(error) {
-            return false;
-        };
-    }
+    // invoked (messageText) {
+    //     try {        
+    //         var botRegex = /(?i)@sleeperbot/;
+    //         var test = messageText && botRegex.test(messageText);
+    //         console.log("RegEx Test: " + test);
+    //         return messageText && botRegex.test(messageText);
+    //     } catch(error) {
+    //         return false;
+    //     };
+    // }
 
     parseMessage (message) {
         console.log(message);
