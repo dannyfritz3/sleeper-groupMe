60758ce4d9bc045901f957fa05a546d7df318cce
Danny Fritz <drfritz@csbsju.edu>
2020-12-15 22:20:46 -0600

    lambda service

--- api/services/LambdaService.js
+++ api/services/LambdaService.js
@@ -14,6 +14,7 @@ module.exports = class LambdaService {
     }
 
     broadcastMatchupLeadersEvent = () => {
-
+        var matchups = sleeperService.getCurrentMatchups();
+        
     }
 }
--- api/services/SleeperService.js
+++ api/services/SleeperService.js
@@ -11,4 +11,18 @@ module.exports = class SleeperService {
         var leagueRosters = await sleeperAdapter.getLeageRosters();
         return leagueRosters.find(roster => roster.owner_id === userId);
     };
+
+    getWeekNumber = async () => {
+        var leagueData = await sleeperAdapter.getLeagueData();
+        return leagueData.settings.leg;
+    };
+
+    getCurrentMatchups = async () => {
+        var currentWeekNumber = this.getWeekNumber();
+        return this.getMatchupsByWeekNumber(currentWeekNumber);
+    };
+
+    getMatchupsByWeekNumber = async (weekNumber) => {
+        return await sleeperAdapter.getLeagueMatchupsByWeekNumber(weekNumber);
+    };
 }
