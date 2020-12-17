5f44f700d11ff4d39418b6deafcf96cf9cb1b551
Danny Fritz <drfritz@csbsju.edu>
2020-12-17 15:44:08 -0600

    testing

--- api/adapters/GroupMeAdapter.js
+++ api/adapters/GroupMeAdapter.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const groupMeApiBaseUrl = "https://api.groupme.com/v3/bots/post";
 const botId = "3e0d38ea2f9b5faf166587dcd9";
+const testBotId = "7645b38769f3684a49319a64ee";
 
 module.exports = class GroupMeAdapter {
     constructor() {};
@@ -8,7 +9,7 @@ module.exports = class GroupMeAdapter {
     postMessage = async (message) => {
         console.log("Posting Message: " + message);
         const url = `${groupMeApiBaseUrl}`
-        var response = await axios.post(url, {"text": message, "bot_id": botId});
+        var response = await axios.post(url, {"text": message, "bot_id": testBotId});
         return response;
     };
 };
