c4bdd79e5738f2ba24b993d8aba3f1efd4c12ca6
Danny Fritz <drfritz@csbsju.edu>
2020-12-17 13:23:34 -0600

    the bot is live with injury updates and matchup updates

--- api/adapters/GroupMeAdapter.js
+++ api/adapters/GroupMeAdapter.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const groupMeApiBaseUrl = "https://api.groupme.com/v3/bots/post";
-const botId = "7645b38769f3684a49319a64ee";
+const botId = "3e0d38ea2f9b5faf166587dcd9";
 
 module.exports = class GroupMeAdapter {
     constructor() {};
