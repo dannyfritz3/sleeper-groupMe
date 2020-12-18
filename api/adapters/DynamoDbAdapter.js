bdf6c0a68cc05de7e1d81f7c3a7bf18c44937c60
Danny Fritz <drfritz@csbsju.edu>
2020-12-18 15:54:15 -0600

    error handling and returning data if found on GetItem

--- api/adapters/DynamoDbAdapter.js
+++ api/adapters/DynamoDbAdapter.js
@@ -10,6 +10,7 @@ module.exports = class DynamoDbAdapter {
         docClient.put(params, function(err, data) {
             if (err) {
                 console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+                throw new Exception(err);
             } else {
                 console.log("PutItem succeeded.");
             }
@@ -17,13 +18,17 @@ module.exports = class DynamoDbAdapter {
     }
 
     getItemFromTable = (params) => {
+        let returnData;
+
         docClient.get(params, function(err, data) {
             if (err) {
                 console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+                throw new Exception(err);
             } else {
                 console.log("GetItem succeeded.");
-                return data;
+                returnData = data;
             }
         });
+        return returnData;
     }
 }
