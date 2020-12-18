f9f99f8ad24071f677dd146e8b2375d58eb78e04
Danny Fritz <drfritz@csbsju.edu>
2020-12-18 13:18:57 -0600

    removed PingService and added route to update players data

--- api/services/PingService.js
+++
@@ -1,8 +0,0 @@
-class PingService {
-    constructor(){};
-    ping = (res) => {
-        res.sendStatus(200);
-    };
-};
-
-module.exports = PingService;
--- routes.js
+++ routes.js
@@ -1,8 +1,9 @@
-const PingService = require('./api/services/PingService.js');
 const SleeperService = require('./api/services/SleeperService.js');
 const GroupMeService = require('./api/services/GroupMeService.js');
 const GroupMeAdapter = require('./api/adapters/GroupMeAdapter.js');
 const SleeperAdapter = require('./api/adapters/SleeperAdapter.js');
+const DynamoDbAdapter = require('./api/adapters/DynamoDbAdapter');
+const AwsService = require('./api/services/AwsService.js');
 const InjuryService = require('./api/services/InjuryService.js');
 const MatchupService = require('./api/services/MatchupService.js');
 const config = require('./config.json');
@@ -10,23 +11,26 @@ const config = require('./config.json');
 module.exports = (server) => {
     var _sleeperAdapter = new SleeperAdapter();
     var _groupMeAdapter = new GroupMeAdapter();
-    var _pingService = new PingService();
+    let _dynamoDbAdapter = new DynamoDbAdapter();
+    let _awsService = new AwsService(_sleeperAdapter, _dynamoDbAdapter);
     var _sleeperService = new SleeperService(_sleeperAdapter);
     var _injuryService = new InjuryService(_groupMeAdapter);
     var _matchupsService = new MatchupService(_sleeperService, _groupMeAdapter, config);
     var _groupMeService = new GroupMeService(_matchupsService, _injuryService, _groupMeAdapter);
 
     server.get(`/`, async (req, res) => {
-        _pingService.ping(res);
+        res.send(200);
     });
 
     server.post(`/groupme/callbackstream`, async (req, res) => {
         try
         {
             _groupMeService.handleCallback(req, res);
+            res.send(200);
         } catch(error)
         {
             console.log(error);
+            res.send(500);
         }
     });
 
@@ -34,32 +38,46 @@ module.exports = (server) => {
         try
         {
             //TODO need to actually create service function for this
-            var result = await _groupMeAdapter.postMessage("cron job works");
-            res.send(result);
+            await _groupMeAdapter.postMessage("cron job works");
+            res.send(200);
         } catch(error)
         {
             console.log(error);
+            res.send(500);
         }
     });
 
     server.get('/broadcast/matchupLeaders', async (req, res) => {
         try
         {
-            var result = await _matchupsService.postMatchupsUpdate();
-            res.send(result);
+            await _matchupsService.postMatchupsUpdate();
+            res.send(200);
         } catch(error)
         {
             console.log(error);
+            res.send(500);
         }
     });
 
     server.get('/broadcast/injuryreport', async (req, res) => {
         try
         {
-            var result = await _injuryService.postInjuryReport();
-            res.send(result);
+            await _injuryService.postInjuryReport();
+            res.send(200);
         } catch (error) {
             console.log(error);
+            res.send(500);
+        }
+    });
+
+    server.get('/aws/updateplayerdb', async (req, res) => {
+        try
+        {
+            await _awsService.updatePlayersTable();
+            res.send(200);   
+        } catch (error) {
+            console.log(error);
+            res.send(500);
         }
     });
 };
